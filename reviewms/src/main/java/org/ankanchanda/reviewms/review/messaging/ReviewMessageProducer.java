da0a46643f84a9c0086648d62c7eb36fdf13c44b
ankanchanda <ankan.chanda74@gmail.com>
2025-04-27 22:48:13 +0530

    setting up producer in company for review

---
+++ reviewms/src/main/java/org/ankanchanda/reviewms/review/messaging/ReviewMessageProducer.java
@@ -0,0 +1,26 @@
+package org.ankanchanda.reviewms.review.messaging;
+
+import org.ankanchanda.reviewms.review.Review;
+import org.ankanchanda.reviewms.review.dto.ReviewMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+public class ReviewMessageProducer {
+    
+    private final RabbitTemplate rabbitTemplate;
+    private static final String ROUTING_KEY = "companyRatingQueue";
+
+    public ReviewMessageProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(Review review) {
+        ReviewMessage reviewMessage = new ReviewMessage();
+        reviewMessage.setId(review.getId());
+        reviewMessage.setTitle(review.getTitle());
+        reviewMessage.setDescription(review.getDescription());
+        reviewMessage.setRating(review.getRating());
+        reviewMessage.setCompanyId(review.getCompanyId());
+
+        rabbitTemplate.convertAndSend(ROUTING_KEY, reviewMessage);
+    }
+}
